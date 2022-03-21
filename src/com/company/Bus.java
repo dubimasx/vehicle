671581873c2cd7631614902464588e61f065418e
dubimasx <dubimasx@gmail.com>
2022-03-21 08:29:30 +0300

    toString(), equals() and hashCode() overriding for class Bus

--- src/com/company/Bus.java
+++ src/com/company/Bus.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Bus extends Vehicle {
     private String engineType;
 
@@ -20,7 +23,31 @@ public class Bus extends Vehicle {
     public void showInfo() {
         System.out.println("Model: " + super.getModel());
         System.out.println("Capacity: " + super.getCapacity());
-        System.out.println("Engine type: " + getEngineType());
+        System.out.println("Engine type: " + this.getEngineType());
 
+    }
+
+    @Override
+    public String toString() {
+        String s = "Model: " + super.getModel() + "\n" +
+                "Capacity: " + super.getCapacity() + "\n" +
+                "Engine type " + getEngineType();
+
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Bus bus = (Bus) o;
+        if (bus.getModel() == super.getModel() && bus.getCapacity() == super.getCapacity() && bus.getRoute() == super.getRoute() && bus.getEngineType() == this.getEngineType() ){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(super.getModel(), super.getCapacity(), super.getRoute(), this.getEngineType());
+        return result;
     }
 }
